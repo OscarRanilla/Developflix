b15445ed2a5c4c742bafda7731dd216b31be7c95
OscarRanilla <oscarjosemanuel89@gmail.com>
2024-11-15 13:51:31 +0100

    EJERCICIO FALTA POR TERMINAR

--- js/script.js
+++ js/script.js
@@ -1,2 +1,11 @@
 import peliculas from './peliculas.js'
 
+const baseURL = "https://image.tmdb.org/t/p/w500";
+
+const generos = {
+    28: "genero-28",
+    53: "genero-53",
+    12: "genero-12"
+};
+function mostrarPeliculasPorGenero() {}
+//NECESITO AYUDA EN ESTE CHALLENGE NO ENTIENDO COMO FILTRAR CADA PELICULA CON SU GENERO
