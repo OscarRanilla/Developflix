dee62070ea38a8b0d69c09687c035f793a34d361
OscarRanilla <oscarjosemanuel89@gmail.com>
2024-11-15 20:43:38 +0100

    EJERCICIO TERMINADO JS CSS

--- js/script.js
+++ js/script.js
@@ -2,10 +2,31 @@ import peliculas from './peliculas.js'
 
 const baseURL = "https://image.tmdb.org/t/p/w500";
 
-const generos = {
-    28: "genero-28",
-    53: "genero-53",
-    12: "genero-12"
-};
-function mostrarPeliculasPorGenero() {}
-//NECESITO AYUDA EN ESTE CHALLENGE NO ENTIENDO COMO FILTRAR CADA PELICULA CON SU GENERO
+function mostrarPeliculasPorGenero(generoId, contenedorId) {
+    const peliculasFiltradas = peliculas.filter(pelicula => pelicula.genre_ids.includes(generoId));
+
+    const contenedor = document.getElementById(contenedorId);
+
+    peliculasFiltradas.forEach(pelicula => {
+
+        const peliculaDiv = document.createElement("div");
+        peliculaDiv.classList.add("pelicula");
+
+        const imagen = document.createElement("img");
+        imagen.src = `${baseURL}${pelicula.poster_path}`;
+        imagen.alt= pelicula.title;
+        imagen.classList.add("pelicula-imagen");
+
+        const titulo = document.createElement("h3");
+        titulo.textContent = pelicula.title;
+        titulo.classList.add("pelicula-titulo");
+
+        peliculaDiv.appendChild(imagen);
+        peliculaDiv.appendChild(titulo);
+
+        contenedor.appendChild(peliculaDiv);
+    });
+}
+console.log(mostrarPeliculasPorGenero(28, "genero-28"));
+console.log(mostrarPeliculasPorGenero(53, "genero-53"));
+console.log(mostrarPeliculasPorGenero(12, "genero-12"));
